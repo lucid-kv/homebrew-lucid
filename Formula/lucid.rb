82c4159900db991252a54692009a8e51a9d759f6
Clint.Network <me@clint.network>
2020-03-23 18:00:49 +0100

    Change the binary to use

--- Formula/lucid.rb
+++ Formula/lucid.rb
@@ -4,8 +4,8 @@
 class Lucid < Formula
   desc "High performance and distributed KV store w/ REST API. 🦀"
   homepage "https://github.com/lucid-kv/lucid"
-  url "https://github.com/lucid-kv/lucid/releases/download/0.1.4/lucid.tar.gz"
-  sha256 "90084db11f84af59a971651ade88909dbac07740e7d24f63daa9aa1ce51f3a50"
+  url "https://github.com/lucid-kv/lucid/releases/download/0.1.4/lucid-macos.tar.gz"
+  sha256 "b41ad6f2b0a50533883665bd6b2f93e7666bfe78c72f0c02670d218b3fd887a9"
   version "0.1.4"
 
   def install
